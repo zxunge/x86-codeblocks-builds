e38a750ddba8d2d99e37967252f6315dc577a31f
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-08-15 09:42:11 +0800

    Initial build script.

---
+++ .ci/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+set -eux
+
+# Build wxWidgets, git
+#git clone https://github.com/wxWidgets/wxWidgets.git
+#pushd wxWidgets/
+
+# Modify setup.h
+#sed -i ＇1568,1568s/0/1/g＇ include/wx/setup.h
+
+#./configure --enable-monolithic --disable-debug --enable-stl
+#make -j$(nproc) install
+#cp -rf include/wx/* /mingw32/include/wx-3.2/wx/
+popd
+
+# Build codeblocks
+git clone https://github.com/arnholm/codeblocks_sfmirror.git
+pushd codeblocks_sfmirror/
+
+# Patch build
+git apply -v ../001-fix-32bit-build.patch
+
+./bootstrap
+./configure --with-contrib-plugins=all --prefix=/opt/codeblocks
+make -j$(nproc) install
