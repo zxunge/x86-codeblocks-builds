1823e09af31121f476ac5a38f8768bb1a7c65df9
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-08-17 10:21:31 +0000

    Code cleanup.

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,31 +2,28 @@
 
 set -eux
 
+########################### Not used ########################
 # Build wxWidgets, git
 #git clone https://github.com/wxWidgets/wxWidgets.git
 #pushd wxWidgets/
 
 # Modify setup.h
-#sed -i ＇1568,1568s/0/1/g＇ include/wx/setup.h
+
 
 #./configure --enable-monolithic --disable-debug --enable-stl
 #make -j$(nproc) install
 #cp -rf include/wx/* /mingw32/include/wx-3.2/wx/
 #popd
+#############################################################
 
 # Build codeblocks
 git clone https://github.com/arnholm/codeblocks_sfmirror.git
 pushd codeblocks_sfmirror/
 
 # Patch build
-#git apply -v ../001-fix-32bit-build.patch
-#sed -i ＇78,78s/WX_CXXFLAGS/WX_CPPFLAGS/g' src/src/Makefile.am
 cp -f ../Makefile.am src/src/
-#cp -f ../lspdiagresultslog.cpp src/plugins/contrib/clangd_client/src/LSPclient/
 
 ./bootstrap
 ./configure --disable-pch --with-contrib-plugins=all,-wxsmith,-wxsmithaui,-wxsmithcontrib,-Valgrind --prefix=/opt/codeblocks
 make -j$(nproc) install
 mv /opt/codeblocks/lib/codeblocks/bin/*.dll /opt/codeblocks/bin/
-
-
