74d2551e9f39cabdcc8c5c5df8740d8d6dde4aed
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-08-19 04:18:32 +0000

    Update build.sh

--- .ci/build.sh
+++ .ci/build.sh
@@ -16,14 +16,15 @@ set -eux
 #popd
 #############################################################
 
-# Build codeblocks
+## Build codeblocks
 git clone https://github.com/arnholm/codeblocks_sfmirror.git
 pushd codeblocks_sfmirror/
 
-# Patch build
-cp -f ../src/Makefile.am src/src/
-cp -f ../plugins/contrib/wxSmith/Makefile.am src/plugins/contrib/wxSmith/
-    # Workaround from msys2: error: definition of static data member 'wxsArrayStringEditorDlg::sm_eventTable' of dllimport'd class
+## Patch build
+#cp -f ../src/Makefile.am src/src/
+#cp -f ../plugins/contrib/wxSmith/Makefile.am src/plugins/contrib/wxSmith/
+git apply -v ../0001-fix-32-bit-build-and-wxSmith.patch
+# Workaround from msys2: error: definition of static data member 'wxsArrayStringEditorDlg::sm_eventTable' of dllimport'd class
 grep -rl "PLUGIN_EXPORT " src/plugins/contrib/wxSmith | xargs -i sed -i "s/PLUGIN_EXPORT //g" {}
 
 ./bootstrap
