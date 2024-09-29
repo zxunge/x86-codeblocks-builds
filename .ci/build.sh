d65aa9cb8d1a61462456a9a60d2d58b7336b220f
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-09-29 22:44:48 +0800

    Make the action run.

--- .ci/build.sh
+++ .ci/build.sh
@@ -21,14 +21,11 @@ git clone https://github.com/arnholm/codeblocks_sfmirror.git
 pushd codeblocks_sfmirror/
 
 ## Obtain revision number
-#TAG=r$(git log --grep="git-svn-id" --max-count=1 | grep "git-svn-id" | awk -F "@" '{print $2}' | awk '{print $1}')
 TAG=r$(git log --graph | grep 'git-svn-id' | head -n 1 | grep -o -e "@\([0-9]*\)" | tr -d '@ ')
 WX_VERSION=$(wx-config --version-full)
 NAME="CodeBlocks svn "${TAG}" and wxWidgets "${WX_VERSION}" Build"
 
 ## Patch build
-#git apply -v ../0001-fix-32-bit-build-and-wxSmith.patch
-#git apply -v ../0001-fix-32-bit-build-wxSmith-and-plugin-loading.patch
 patch -Nbp1 -i ../004-disable-parallel-make-for-SmartIndent.patch 
 patch -Nbp1 -i ../005-codeblocks-plugin-fix.patch 
 patch -Nbp1 -i ../006-Update-ax_cxx_compile_stdcxx.m4.patch 
@@ -41,7 +38,7 @@ patch -Nbp1 -i ../008-Use-WX_CPPFLAGS-instead-of-WX_CXXFLAGS-in-Makefile.a.patch
 ./configure --disable-pch --with-contrib-plugins=all,-Valgrind --prefix=/opt/codeblocks
 make -j$(nproc) install
 mv /opt/codeblocks/lib/codeblocks/bin/*.dll /opt/codeblocks/bin/
-mv /opt/codeblocks/lib/*.dll /opt/codeblocks/bin/
+#mv /opt/codeblocks/lib/*.dll /opt/codeblocks/bin/
 
 if [[ -v GITHUB_WORKFLOW ]]; then
   echo "RELEASE_NAME=${NAME}" >> $GITHUB_OUTPUT
