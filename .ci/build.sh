b381217a14aa93ffeccc81294509865bb7cec2c5
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-09-30 19:24:00 +0800

    Code cleanup.

--- .ci/build.sh
+++ .ci/build.sh
@@ -16,7 +16,7 @@ set -eux
 #popd
 #############################################################
 
-## Build codeblocks
+## Clone CodeBlocks' source
 git clone https://github.com/arnholm/codeblocks_sfmirror.git
 pushd codeblocks_sfmirror/
 
@@ -25,15 +25,7 @@ TAG=r$(git log --graph | grep 'git-svn-id' | head -n 1 | grep -o -e "@\([0-9]*\)
 WX_VERSION=$(wx-config --version-full)
 NAME="CodeBlocks svn "${TAG}" and wxWidgets "${WX_VERSION}" Build"
 
-## Patch build
-patch -Nbp1 -i ../004-disable-parallel-make-for-SmartIndent.patch 
-patch -Nbp1 -i ../005-codeblocks-plugin-fix.patch 
-patch -Nbp1 -i ../006-Update-ax_cxx_compile_stdcxx.m4.patch 
-patch -Nbp1 -i ../007-makefile-wxsmith-plugin-export-fix.patch 
-patch -Nbp1 -i ../008-Use-WX_CPPFLAGS-instead-of-WX_CXXFLAGS-in-Makefile.a.patch
-# Workaround from msys2: error: definition of static data member 'wxsArrayStringEditorDlg::sm_eventTable' of dllimport'd class
-#grep -rl "PLUGIN_EXPORT " src/plugins/contrib/wxSmith | xargs -i sed -i "s/PLUGIN_EXPORT //g" {}
-
+## Build CodeBlocks
 ./bootstrap
 ./configure --disable-pch --with-contrib-plugins=all,-Valgrind --prefix=/opt/codeblocks
 make -j$(nproc) install
